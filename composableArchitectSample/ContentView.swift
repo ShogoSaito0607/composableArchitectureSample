5d75bf5bcd8e65249d4e94236a2ad132125c3bf3
Shogo Saito <shogo.saito@bitkey.jp>
2020-05-29 13:56:29 +0900

    Initial Commit

---
+++ composableArchitectSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  composableArchitectSample
+//
+//  Created by ShogoSaito on 2020/05/29.
+//  Copyright © 2020 bitkey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
